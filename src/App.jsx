a1cebd7864d4d9be479a77ccea2e39a799562846
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2026-02-18 23:54:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import ApiDocs from './pages/ApiDocs'
 import Terms from './pages/Terms'
+import Footer from './components/Footer'
 import { useEffect } from 'react'
 import i18n from './i18n'
 
@@ -11,21 +12,20 @@ function AppContent() {
   const { lang } = useParams()
 
   useEffect(() => {
-    if (lang === 'en' || lang === 'id') {
-      i18n.changeLanguage(lang)
-    }
+    if (lang === 'en' || lang === 'id') i18n.changeLanguage(lang)
   }, [lang])
 
   return (
-    <div className="min-h-screen bg-zinc-950 text-zinc-100 font-sans">
+    <div className="min-h-screen bg-zinc-950 text-zinc-100 font-sans flex flex-col">
       <Header />
-      <main className="pt-16">
+      <main className="pt-16 flex-1">
         <Routes>
           <Route path="/:lang/\~" element={<Home />} />
           <Route path="/:lang/docs" element={<ApiDocs />} />
           <Route path="/:lang/terms" element={<Terms />} />
         </Routes>
       </main>
+      <Footer />
       <Toaster position="top-center" richColors closeButton />
     </div>
   )
